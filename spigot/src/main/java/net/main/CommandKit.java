5d17d266946bf89d32fec249788939d0cbc6b5d7
Jurij-Christopher Becker <inf3732@hs-worms.de>
2024-02-13 16:33:40 +0100

    Want to endmyself. solved coreprotect resolve issue?

--- spigot/src/main/java/net/main/CommandKit.java
+++ spigot/src/main/java/net/main/CommandKit.java
@@ -6,6 +6,30 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import net.coreprotect.CoreProtect;
+import net.coreprotect.CoreProtectAPI;
+
+private CoreProtectAPI getCoreProtect() {
+    Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
+
+    // Check that CoreProtect is loaded
+    if (plugin == null || !(plugin instanceof CoreProtect)) {
+        return null;
+    }
+
+    // Check that the API is enabled
+    CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+    if (CoreProtect.isEnabled() == false) {
+        return null;
+    }
+
+    // Check that a compatible version of the API is loaded
+    if (CoreProtect.APIVersion() < 9) {
+        return null;
+    }
+
+    return CoreProtect;
+}
 
 public class CommandKit implements CommandExecutor {
 
--- spigot/src/main/java/net/main/SpigotExample.java
+++ spigot/src/main/java/net/main/SpigotExample.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
+
 public final class SpigotExample extends JavaPlugin {
 
     @Override
