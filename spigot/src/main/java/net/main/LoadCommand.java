49175d42527e55b435dbdc98d83e692a4148e5e1
Felix Schlembach <felix.schlembach@gmx.de>
2024-02-14 11:19:57 +0100

    update

--- spigot/src/main/java/net/main/LoadCommand.java
+++ spigot/src/main/java/net/main/LoadCommand.java
@@ -6,11 +6,13 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class HelloBlock implements CommandExecutor {
+public class LoadCommand implements CommandExecutor {
     
+    string data[] = 
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        sender.sendMessage("Hello World!");
+        sender.sendMessage("Loading commit...");
 
         Location loc = new Location(Bukkit.getWorld("world"), Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
         loc.getBlock().setBlockData(Bukkit.createBlockData("minecraft:grass_block[snowy=true]"), true);
