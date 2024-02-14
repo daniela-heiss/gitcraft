13fd48c0303cb4c547be9fe824954d0a0eb962e6
Jurij-Christopher Becker <inf3732@hs-worms.de>
2024-02-14 10:11:02 +0100

    solved issues. Coreprotect API is usable

--- spigot/src/main/java/net/main/CommandKit.java
+++ spigot/src/main/java/net/main/CommandKit.java
@@ -1,56 +1,48 @@
 package net.main;
 
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import net.coreprotect.CoreProtect;
 import net.coreprotect.CoreProtectAPI;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import java.util.Objects;
 
-private CoreProtectAPI getCoreProtect() {
-    Plugin plugin = getServer().getPluginManager().getPlugin("CoreProtect");
 
-    // Check that CoreProtect is loaded
-    if (plugin == null || !(plugin instanceof CoreProtect)) {
-        return null;
-    }
-
-    // Check that the API is enabled
-    CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
-    if (CoreProtect.isEnabled() == false) {
-        return null;
-    }
-
-    // Check that a compatible version of the API is loaded
-    if (CoreProtect.APIVersion() < 9) {
-        return null;
-    }
-
-    return CoreProtect;
-}
 
 public class CommandKit implements CommandExecutor {
 
     @Override
     public boolean  onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (sender instanceof Player player) {
-
-            // Create a new ItemStack (type: diamond)
-            ItemStack diamond = new ItemStack(Material.DIAMOND);
-
-            // Create a new ItemStack (type: brick)
-            ItemStack bricks = new ItemStack(Material.BRICK);
-
-            // Set the amount of the ItemStack
-            bricks.setAmount(20);
-
-            // Give the player our items (comma-seperated list of all ItemStack)
-            player.getInventory().addItem(bricks, diamond);
+        CoreProtectAPI api = getCoreProtect();
+        if (api != null){ // Ensure we have access to the API
+            api.testAPI(); // Will print out "[CoreProtect] API test successful." in the console.
         }
 
         // If the player (or console) uses our command correct, we can return true
         return true;
+    }
+    private CoreProtectAPI getCoreProtect() {
+        Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("CoreProtect");
+
+        // Check that CoreProtect is loaded
+        if (plugin == null || !(plugin instanceof CoreProtect)) {
+            return null;
+        }
+
+        // Check that the API is enabled
+        CoreProtectAPI CoreProtect = ((CoreProtect) plugin).getAPI();
+        if (CoreProtect.isEnabled() == false) {
+            return null;
+        }
+
+        // Check that a compatible version of the API is loaded
+        if (CoreProtect.APIVersion() < 9) {
+            return null;
+        }
+
+        return CoreProtect;
     }
 }
