cdec0173d76188f67d0caa48aa12bda83ddff034
Max Mueller <max97m2@gmail.com>
2024-02-15 00:15:56 +0100

    initial maven and orm setup

---
+++ spigot/src/main/java/top/mavenspigotdbtest/DatabaseManager.java
@@ -0,0 +1,32 @@
+package top.mavenspigotdbtest;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/minecraft";
+    private static final String DATABASE_USERNAME = "mc";
+    private static final String DATABASE_PASSWORD = "passwd";
+
+    private ConnectionSource connectionSource;
+
+    public void initializeDatabase() throws SQLException {
+        connectionSource = new JdbcConnectionSource(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+        // Create tables if they don't exist
+        TableUtils.createTableIfNotExists(connectionSource, PlayerEntity.class);
+    }
+
+    public void closeConnection() throws Exception {
+        if (connectionSource != null) {
+            connectionSource.close();
+        }
+    }
+
+    public PlayerDao getPlayerDao() throws SQLException {
+        return new PlayerDao(connectionSource);
+    }
+}
---
+++ spigot/src/main/java/top/mavenspigotdbtest/PlayerEntity.java
@@ -0,0 +1,14 @@
+package top.mavenspigotdbtest;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "players")
+public class PlayerEntity {
+    @DatabaseField(generatedId  = true)
+    public int id;
+
+    @DatabaseField
+    public String name;
+
+}
