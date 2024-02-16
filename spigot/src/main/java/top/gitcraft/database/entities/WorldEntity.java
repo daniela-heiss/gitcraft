181d4a359748de091b1732a17439f4d998fd9525
Max Müller <max97m2@gmail.com>
2024-02-16 10:23:53 +0100

    Refactor rowid field in WorldEntity class

--- spigot/src/main/java/top/gitcraft/database/entities/WorldEntity.java
+++ spigot/src/main/java/top/gitcraft/database/entities/WorldEntity.java
@@ -6,8 +6,8 @@ import com.j256.ormlite.table.DatabaseTable;
 @DatabaseTable(tableName = "co_world")
 public class WorldEntity {
 
-    @DatabaseField(generatedId = true)
-    public int rowid;
+    @DatabaseField(generatedId = true, columnName = "rowid")
+    public int rowId;
 
     @DatabaseField
     public int id;
