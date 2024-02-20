1153090535db3a02fd803b92cc477d43fda4e3bf
Daniela Heiß <inf3741@hs-worms.de>
2024-02-20 09:41:36 +0100

    Fixed an issue in GitCraft file

--- spigot/src/main/java/top/gitcraft/GitCraft.java
+++ spigot/src/main/java/top/gitcraft/GitCraft.java
@@ -4,6 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import top.gitcraft.commands.LoadCommand;
 import top.gitcraft.database.DatabaseManager;
+
+import java.sql.SQLException;
 import java.util.Objects;
 
 public final class GitCraft extends JavaPlugin {
@@ -12,15 +14,19 @@ public final class GitCraft extends JavaPlugin {
         // Plugin startup logic
         //send hello message
         getLogger().info("Hello, SpigotMC!");
-        Objects.requireNonNull(this.getCommand("load")).setExecutor(new LoadCommand());
-        //initialize database
         try {
+            Objects.requireNonNull(this.getCommand("load")).setExecutor(new LoadCommand());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //initialize database
+       /* try {
             DatabaseManager databaseManager = new DatabaseManager();
             databaseManager.initializeDatabase();
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     @Override
