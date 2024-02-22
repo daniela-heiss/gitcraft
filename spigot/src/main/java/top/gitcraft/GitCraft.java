31223d9dbb2eb9ffca10f1e10376e6af559adeff
Max Müller <max97m2@gmail.com>
2024-02-22 13:29:21 +0100

    removed unnecessary platform test

--- spigot/src/main/java/top/gitcraft/GitCraft.java
+++ spigot/src/main/java/top/gitcraft/GitCraft.java
@@ -16,12 +16,6 @@ public final class GitCraft extends JavaPlugin {
         getLogger().info("Hello, SpigotMC!");
 
         try {
-            Platform platform = WorldEdit.getInstance().getPlatformManager().getPlatforms().getLast();
-            if (platform != null) {
-                getLogger().info("WorldEdit platform: " + platform.getPlatformName());
-            } else {
-                getLogger().warning("WorldEdit platform is null!");
-            }
             Objects.requireNonNull(this.getCommand("load")).setExecutor(new LoadCommand());
             Objects.requireNonNull(this.getCommand("gcsave")).setExecutor(new GCSave());
             Objects.requireNonNull(this.getCommand("setBlockExample")).setExecutor(new WETestCommand());
