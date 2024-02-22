fa6eb67fa405a5aa2b3569dfae03504a8bcf86a5
Felix Schlembach <felix.schlembach@gmx.de>
2024-02-23 00:26:40 +0100

    Dynamically load data into menus

--- spigot/src/main/java/top/gitcraft/commands/BranchCreateListCommand.java
+++ spigot/src/main/java/top/gitcraft/commands/BranchCreateListCommand.java
@@ -1,10 +1,13 @@
 package top.gitcraft.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class BranchCreateListCommand implements CommandExecutor {
 
@@ -17,8 +20,8 @@ public class BranchCreateListCommand implements CommandExecutor {
                     + "{\"text\":\"\\u2550\\u2550\",\"bold\":true,\"color\":\"white\"},"
                     + "{\"text\":\" Git\",\"bold\":true,\"color\":\"red\"},"
                     + "{\"text\":\"Craft\",\"bold\":true,\"color\":\"gold\"},"
-                    + "{\"text\":\" \\u2550\\u2550\\n\\nBranch List\",\"bold\":true,\"color\":\"white\"},"
-                    + "{\"text\":\"\\n\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\",\"bold\":true},"
+                    + "{\"text\":\" \\u2550\\u2550\\n\\nBranch Create List\",\"bold\":true,\"color\":\"white\"},"
+                    + "{\"text\":\"\\n\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\",\"bold\":true},"
                     + "{\"text\":\"\\n\\n\\u2554\"},"
                     + "{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate world\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create world\"}},"
                     + "{\"text\":\"CREATE\",\"bold\":true,\"color\":\"aqua\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate world\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create world\"}},"
@@ -43,11 +46,63 @@ public class BranchCreateListCommand implements CommandExecutor {
 
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + sender.getName() + " " + jsonMessage);
 
+//            List<World> worlds = Bukkit.getWorlds();
+//            String jsonMessage = generateDynamicMessage(worlds);
+//
+//            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + sender.getName() + " " + jsonMessage);
+
             return true;
         }
         else{
             sender.sendMessage("You must be a player to use this command!");
             return false;
         }
+    }
+    private String generateDynamicMessage(List<World> worlds) {
+        StringBuilder jsonMessage = new StringBuilder("[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"\\u2550\\u2550\",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\" Git\",\"bold\":true,\"color\":\"red\"},"
+                + "{\"text\":\"Craft\",\"bold\":true,\"color\":\"gold\"},"
+                + "{\"text\":\" \\u2550\\u2550\\n\\nBranch List\",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"\\n\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\n\",\"bold\":true}");
+
+        jsonMessage.append(",{\"text\":\"\\n\",\"bold\":true},"
+                + "{\"text\":\"\\u2554\",\"color\":\"white\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(worlds.get(0).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(worlds.get(0).getName()).append("\"}},");
+        jsonMessage.append("{\"text\":\"CREATE\",\"bold\":true,\"color\":\"aqua\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(worlds.get(0).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(worlds.get(0).getName()).append("\"}},");
+        jsonMessage.append("{\"text\":\"] ").append(worlds.get(0).getName()).append("\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(worlds.get(0).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(worlds.get(0).getName()).append("\"}}");
+
+        worlds.remove(0);
+        World lastWorld = worlds.get(worlds.size()-1);
+        if(!worlds.isEmpty()){
+            worlds.remove(worlds.size()-1);
+        }
+
+        for (World world : worlds) {
+            String worldName = world.getName();
+            jsonMessage.append(",{\"text\":\"\\n\",\"bold\":true},"
+                    + "{\"text\":\"\\u2560\",\"color\":\"white\"},"
+                    + "{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(worldName).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(worldName).append("\"}},");
+            jsonMessage.append("{\"text\":\"CREATE\",\"bold\":true,\"color\":\"aqua\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(worldName).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(worldName).append("\"}},");
+            jsonMessage.append("{\"text\":\"] ").append(worldName).append("\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(worldName).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(worldName).append("\"}}");
+        }
+        if(!worlds.isEmpty()){
+            jsonMessage.append(",{\"text\":\"\\n\",\"bold\":true},"
+                    + "{\"text\":\"\\u255a\",\"color\":\"white\"},"
+                    + "{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(lastWorld.getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(lastWorld.getName()).append("\"}},");
+            jsonMessage.append("{\"text\":\"CREATE\",\"bold\":true,\"color\":\"aqua\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(lastWorld.getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(lastWorld.getName()).append("\"}},");
+            jsonMessage.append("{\"text\":\"] ").append(lastWorld.getName()).append("\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchcreate ").append(lastWorld.getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to create ").append(lastWorld.getName()).append("\"}}");
+
+        }
+
+        jsonMessage.append(",{\"text\":\"\\n\\n\\n\",\"bold\":true},"
+                + "{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchmenu\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Open branch menu\"}},"
+                + "{\"text\":\"Branch Menu\",\"bold\":true,\"color\":\"yellow\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchmenu\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Open branch menu\"}},"
+                + "{\"text\":\"]\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchmenu\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Open branch menu\"}}");
+
+        jsonMessage.append(",{\"text\":\"\\n \"}]");
+
+        return jsonMessage.toString();
     }
 }
--- spigot/src/main/java/top/gitcraft/commands/BranchListCommand.java
+++ spigot/src/main/java/top/gitcraft/commands/BranchListCommand.java
@@ -1,11 +1,13 @@
 package top.gitcraft.commands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.List;
 import java.util.Objects;
 
 public class BranchListCommand implements CommandExecutor {
@@ -29,4 +31,53 @@ public class BranchListCommand implements CommandExecutor {
         return true;
 
     }
+//
+//    public String generateDynamicMessage(CommandSender sender, List<World> worlds, String action, String color) {
+//        String corner = "{\"text\":\"\\n\",\"bold\":true},{\"text\":\"\\u2554\",\"color\":\"white\"}";
+//        String middle = "{\"text\":\"\\n\",\"bold\":true},{\"text\":\"\\u2560\",\"color\":\"white\"}";
+//        String end = "{\"text\":\"\\n\",\"bold\":true},{\"text\":\"\\u255a\",\"color\":\"white\"}";
+//
+//        StringBuilder jsonMessage = new StringBuilder("[\"\","
+//                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+//                + "{\"text\":\"\\u2550\\u2550\",\"bold\":true,\"color\":\"white\"},"
+//                + "{\"text\":\" Git\",\"bold\":true,\"color\":\"red\"},"
+//                + "{\"text\":\"Craft\",\"bold\":true,\"color\":\"gold\"},"
+//                + "{\"text\":\" \\u2550\\u2550\\n\\nBranch List\",\"bold\":true,\"color\":\"white\"},"
+//                + "{\"text\":\"\\n\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\u2550\\n\",\"bold\":true}");
+//
+//        if (!worlds.isEmpty()) {
+//            jsonMessage.append(corner)
+//                    .append("{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(worlds.get(0).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(worlds.get(0).getName()).append("\"}}");
+//            jsonMessage.append(",{\"text\":\"").append(action.toUpperCase()).append("\",\"bold\":true,\"color\":\"").append(color).append("\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(worlds.get(0).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(worlds.get(0).getName()).append("\"}}");
+//            jsonMessage.append(",{\"text\":\"] ").append(worlds.get(0).getName()).append("\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(worlds.get(0).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(worlds.get(0).getName()).append("\"}}");
+//
+//            worlds.remove(0);
+//
+//            for (World world : worlds) {
+//                jsonMessage.append(middle)
+//                        .append("{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(world.getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(world.getName()).append("\"}}");
+//                jsonMessage.append(",{\"text\":\"").append(action.toUpperCase()).append("\",\"bold\":true,\"color\":\"").append(color).append("\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(world.getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(world.getName()).append("\"}}");
+//                jsonMessage.append(",{\"text\":\"] ").append(world.getName()).append("\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(world.getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(world.getName()).append("\"}}");
+//            }
+//
+//            if (!worlds.isEmpty()) {
+//                jsonMessage.append(end)
+//                        .append("{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(worlds.get(worlds.size() - 1).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(worlds.get(worlds.size() - 1).getName()).append("\"}}");
+//                jsonMessage.append(",{\"text\":\"").append(action.toUpperCase()).append("\",\"bold\":true,\"color\":\"").append(color).append("\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(worlds.get(worlds.size() - 1).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(worlds.get(worlds.size() - 1).getName()).append("\"}}");
+//                jsonMessage.append(",{\"text\":\"] ").append(worlds.get(worlds.size() - 1).getName()).append("\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranch").append(action).append(" ").append(worlds.get(worlds.size() - 1).getName()).append("\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Click to ").append(action).append(" ").append(worlds.get(worlds.size() - 1).getName()).append("\"}}");
+//            }
+//        }
+//
+//        jsonMessage.append(",{\"text\":\"\\n\\n\\n\",\"bold\":true},"
+//                + "{\"text\":\"[\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchmenu\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Open branch menu\"}},"
+//                + "{\"text\":\"Branch Menu\",\"bold\":true,\"color\":\"yellow\",\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchmenu\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Open branch menu\"}},"
+//                + "{\"text\":\"]\",\"bold\":true,\"clickEvent\":{\"action\":\"run_command\",\"value\":\"/gcbranchmenu\"},\"hoverEvent\":{\"action\":\"show_text\",\"contents\":\"Open branch menu\"}}");
+//
+//        jsonMessage.append(",{\"text\":\"\\n \"}]");
+//
+//        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + sender.getName() + " " + jsonMessage);
+//
+//        return jsonMessage.toString();
+//    }
+
 }
