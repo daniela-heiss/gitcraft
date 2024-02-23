04975c3041c04fa25828d269fcea34455ec514fe
Max Müller <max97m2@gmail.com>
2024-02-23 09:38:05 +0100

    linting

--- spigot/src/main/java/top/gitcraft/database/daos/WorldDao.java
+++ spigot/src/main/java/top/gitcraft/database/daos/WorldDao.java
@@ -2,13 +2,14 @@ package top.gitcraft.database.daos;
 
 import com.j256.ormlite.dao.BaseDaoImpl;
 import com.j256.ormlite.support.ConnectionSource;
+import org.bukkit.World;
 import top.gitcraft.database.entities.WorldEntity;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public class WorldDao extends BaseDaoImpl<WorldEntity, Integer> {
-    
+
 
     public WorldDao(ConnectionSource connectionSource) throws SQLException {
         super(connectionSource, WorldEntity.class);
@@ -40,6 +41,6 @@ public class WorldDao extends BaseDaoImpl<WorldEntity, Integer> {
     }
 
     public void addColumn(String columnName, String columnType) throws SQLException {
-        executeRaw("ALTER TABLE co_world ADD COLUMN" + " " + columnName + " "+ columnType);
+        executeRaw("ALTER TABLE co_world ADD COLUMN" + " " + columnName + " " + columnType);
     }
 }
