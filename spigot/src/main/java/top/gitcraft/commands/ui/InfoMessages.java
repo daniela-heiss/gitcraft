e2c8f4fd9f44aedd75592a00124c404f3569524d
Felix Schlembach <felix.schlembach@gmx.de>
2024-02-25 19:02:31 +0100

    Consolidated JSON info messages into dedicated methods

---
+++ spigot/src/main/java/top/gitcraft/commands/ui/InfoMessages.java
@@ -0,0 +1,118 @@
+package top.gitcraft.commands.ui;
+
+public class InfoMessages {
+    String infoNoWorldNameProvided(){
+        /*
+         * [i] Please provide a world name
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"blue\"},"
+                + "{\"text\":\"i\",\"bold\":true},{\"text\":\"] \","
+                + "\"bold\":true,\"color\":\"blue\"},"
+                + "{\"text\":\"Please provide a world name\",\"bold\":true},"
+                + "{\"text\":\"\\n \"}]";
+    }
+    String infoCreatingBranch(String worldName) {
+        /*
+         * [i] Creating <worldName> ...
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"aqua\"},"
+                + "{\"text\":\"i\",\"bold\":true},"
+                + "{\"text\":\"]\",\"bold\":true,\"color\":\"aqua\"},"
+                + "{\"text\":\" Creating \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"" + worldName + "\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" ... \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"\\n \"}]";
+    }
+    String infoBranchCreated(String worldName) {
+        /*
+         * [i] <worldName> created
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"aqua\"},"
+                + "{\"text\":\"i\",\"bold\":true},"
+                + "{\"text\":\"] \",\"bold\":true,\"color\":\"aqua\"},"
+                + "{\"text\":\"" + worldName + "\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" created \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"\\n \"}]";
+    }
+    String infoJoiningWorld(String worldName) {
+        /*
+         * [i] Joining <worldName> ...
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\"i\",\"bold\":true},"
+                + "{\"text\":\"]\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" Joining \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"" + worldName + "\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" ... \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"\\n \"}]";
+    }
+    String infoWorldJoined(String worldName) {
+        /*
+         * [i] <worldName> created
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"aqua\"},"
+                + "{\"text\":\"i\",\"bold\":true},"
+                + "{\"text\":\"] \",\"bold\":true,\"color\":\"aqua\"},"
+                + "{\"text\":\"" + worldName + "\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" joined \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"\\n \"}]";
+    }
+    String infoWorldIsProtected(String worldName) {
+        /*
+         * [!] <worldName> is protected and will not be deleted
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"red\"},"
+                + "{\"text\":\"!\",\"bold\":true},"
+                + "{\"text\":\"]\",\"bold\":true,\"color\":\"red\"},"
+                + "{\"text\":\"" + worldName + "\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" is protected and will not be deleted\\n\",\"bold\":true}]";
+    }
+    String infoDeletingWorld(String worldName) {
+        /*
+         * [i] Deleting <worldName> ...
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"red\"},"
+                + "{\"text\":\"i\",\"bold\":true},"
+                + "{\"text\":\"]\",\"bold\":true,\"color\":\"red\"},"
+                + "{\"text\":\" Deleting \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\""+ worldName +"\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" ... \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"\\n \"}]";
+    }
+    String infoWorldDeleted(String worldName) {
+        /*
+         * [i] <worldName> deleted
+         *
+         */
+        return "[\"\","
+                + "{\"text\":\"\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\\n\"},"
+                + "{\"text\":\"[\",\"bold\":true,\"color\":\"red\"},"
+                + "{\"text\":\"i\",\"bold\":true},"
+                + "{\"text\":\"] \",\"bold\":true,\"color\":\"red\"},"
+                + "{\"text\":\""+ worldName +"\",\"bold\":true,\"color\":\"green\"},"
+                + "{\"text\":\" deleted \",\"bold\":true,\"color\":\"white\"},"
+                + "{\"text\":\"\\n \"}]";
+    }
+
+}
