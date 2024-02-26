5f09433781b1995d2792e8a14442dab7cca00837
Felix Schlembach <felix.schlembach@gmx.de>
2024-02-26 11:12:34 +0100

    Removed unused database functions

--- spigot/src/main/java/top/gitcraft/GitCraft.java
+++ spigot/src/main/java/top/gitcraft/GitCraft.java
@@ -17,12 +17,12 @@ public final class GitCraft extends JavaPlugin {
         Objects.requireNonNull(this.getCommand("gcbranchmenu")).setExecutor(new BranchMenuCommand());
         Objects.requireNonNull(this.getCommand("gcbranch")).setExecutor(new BranchCommand());
         Objects.requireNonNull(this.getCommand("gcbranchjoin")).setExecutor(new BranchJoinCommand());
+        Objects.requireNonNull(this.getCommand("gcbranchcreate")).setExecutor(new BranchCreateCommand());
         Objects.requireNonNull(this.getCommand("gcbranchdelete")).setExecutor(new BranchDeleteCommand());
         Objects.requireNonNull(this.getCommand("gcWETest")).setExecutor(new WETestCommand());
 
         try {
             Objects.requireNonNull(this.getCommand("merge")).setExecutor(new MergeCommand());
-            Objects.requireNonNull(this.getCommand("gcbranchcreate")).setExecutor(new BranchCreateCommand());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
