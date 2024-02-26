18448fd56fb5185af5d0874287a4ad0e4d3ca2b0
Felix Schlembach <felix.schlembach@gmx.de>
2024-02-26 16:56:43 +0100

    Created util function for tellraw commands

---
+++ spigot/src/main/java/top/gitcraft/utils/ExecuteConsoleCommand.java
@@ -0,0 +1,11 @@
+package top.gitcraft.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import top.gitcraft.ui.components.Menu;
+
+public class ExecuteConsoleCommand {
+    public static void dispatchTellRawCommand(Player player, String jsonMessage){
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + player.getName() + " " + jsonMessage);
+    }
+}
