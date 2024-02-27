d3d6599e9927ef47e8419929de583be181860128
Daniela Heiß <inf3741@hs-worms.de>
2024-02-27 09:44:04 +0100

    Small updates to Box Class

--- spigot/src/main/java/top/gitcraft/commands/SelectionParticleCommand.java
+++ spigot/src/main/java/top/gitcraft/commands/SelectionParticleCommand.java
@@ -4,22 +4,23 @@ import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.CuboidRegion;
 
 public class SelectionParticleCommand {
-    class Block {
+    class Box {
         private final BlockVector3 a;
-        private final BlockVector3 b = null;
-        private final BlockVector3 c = null;
-        private final BlockVector3 d = null;
-        private final BlockVector3 e = null;
-        private final BlockVector3 f = null;
+        private final BlockVector3 b;
+        private final BlockVector3 c;
+        private final BlockVector3 d;
+        private final BlockVector3 e;
+        private final BlockVector3 f;
         private final BlockVector3 g;
-        private final BlockVector3 h = null;
+        private final BlockVector3 h;
 
 
-        Block(BlockVector3 pos1, BlockVector3 pos2){
+        Box(BlockVector3 pos1, BlockVector3 pos2){
             a = pos1;
             g = pos2;
 
-            b = new BlockVector3(a.getX(), a.getY(), g.getZ());
+            b = BlockVector3.at(a.getX(), a.getY(), g.getZ());
+            c = BlockVector3.at(a.getX(), a.getY(), g.getZ());
 
 
 
