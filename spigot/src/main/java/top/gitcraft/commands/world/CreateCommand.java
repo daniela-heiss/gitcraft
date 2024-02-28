e271f112d9ef4ff3e9f43146c1ba5039b7468736
Max Müller <max97m2@gmail.com>
2024-02-28 10:26:58 +0100

    Refactor CreateCommand class to improve code readability and maintainability

--- spigot/src/main/java/top/gitcraft/commands/world/CreateCommand.java
+++ spigot/src/main/java/top/gitcraft/commands/world/CreateCommand.java
@@ -11,18 +11,11 @@ import top.gitcraft.GitCraft;
 
 import java.time.Instant;
 
-import java.util.Objects;
-
 import static top.gitcraft.ui.components.Info.infoCreatingWorld;
 import static top.gitcraft.ui.components.Info.infoWorldCreated;
 import static top.gitcraft.utils.methods.ExecuteConsoleCommand.dispatchTellRawCommand;
 
 public class CreateCommand implements CommandExecutor {
-    private final GitCraft gitCraft;
-    public CreateCommand(GitCraft gitCraft) {
-        this.gitCraft = gitCraft;
-    }
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
@@ -32,61 +25,75 @@ public class CreateCommand implements CommandExecutor {
         Player player = (Player) sender;
 
         String currentWorldName = player.getWorld().getName();
-        String clonedWorldName = generateWorldName(player, currentWorldName);
+        String worldName = args[0];
+        Boolean doTeleport = Boolean.parseBoolean(args[1]);
 
-        switch (args.length){
-            // Clone world, give name and don't teleport if doTeleport == "false"
-            case 2:
-                createWorldAndTp(player, clonedWorldName, args[1]);
-                return true;
-            // Clone world and give name
-            case 1:
-                createWorld(player, args[0]);
-                return true;
-            // Clone world and give random name
-            default:
-                createWorld(player, clonedWorldName);
-                return true;
+        //if no world name use clone world name
+        if (worldName == null) {
+            worldName = generateWorldName(currentWorldName);
         }
+
+        if (doTeleport) {
+            return cloneWorld(player, worldName);
+        }
+        return cloneWorldAndTeleport(player, worldName);
     }
 
-    public void createWorld(Player player, String clonedWorldName) {
+    /**
+     * This method is clones the world the player is currently in and gives it a new name.
+     *
+     * @param player    The player who executed the command
+     * @param worldName The name of the new world
+     */
+    public boolean cloneWorld(Player player, String worldName) {
         MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
         MVWorldManager worldManager = core.getMVWorldManager();
 
-        dispatchTellRawCommand(player, infoCreatingWorld(clonedWorldName));
+        dispatchTellRawCommand(player, infoCreatingWorld(worldName));
 
-        Bukkit.getScheduler().runTask(gitCraft, () -> {
+        Bukkit.getScheduler().runTask(GitCraft.getPlugin(GitCraft.class), () -> {
             // Clone the world after the message is sent
-            worldManager.cloneWorld(player.getWorld().getName(), clonedWorldName);
+            worldManager.cloneWorld(player.getWorld().getName(), worldName);
 
             // Send the second message after the cloning operation is completed
-            dispatchTellRawCommand(player, infoWorldCreated(clonedWorldName));
+            dispatchTellRawCommand(player, infoWorldCreated(worldName));
 
-            new JoinCommand(gitCraft).joinWorldAtCurrentLocation(player, clonedWorldName, "true");
         });
+        return true;
     }
 
-    public void createWorldAndTp(Player player, String clonedWorldName, String doTeleport) {
-        if (Objects.equals(doTeleport, "false")) {
-            MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
-            MVWorldManager worldManager = core.getMVWorldManager();
+    /**
+     * This method is clones the world the player is currently in and gives it a new name.
+     * It also teleports the player to the new world.
+     *
+     * @param player    The player who executed the command
+     * @param worldName The name of the new world
+     */
+    public boolean cloneWorldAndTeleport(Player player, String worldName) {
+        MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+        MVWorldManager worldManager = core.getMVWorldManager();
 
-            dispatchTellRawCommand(player, infoCreatingWorld(clonedWorldName));
+        dispatchTellRawCommand(player, infoCreatingWorld(worldName));
 
-            Bukkit.getScheduler().runTask(gitCraft, () -> {
-                // Clone the world after the message is sent
-                worldManager.cloneWorld(player.getWorld().getName(), clonedWorldName);
+        Bukkit.getScheduler().runTask(GitCraft.getPlugin(GitCraft.class), () -> {
+            // Clone the world after the message is sent
+            worldManager.cloneWorld(player.getWorld().getName(), worldName);
 
-                // Send the second message after the cloning operation is completed
-                dispatchTellRawCommand(player, infoWorldCreated(clonedWorldName));
-            });
-        } else {
-            createWorld(player, clonedWorldName);
-        }
+            // Send the second message after the cloning operation is completed
+            dispatchTellRawCommand(player, infoWorldCreated(worldName));
+        });
+        new JoinCommand().joinWorldAtCurrentLocation(player, worldName);
+        return true;
+
     }
 
-    private String generateWorldName(Player player, String worldName) {
+    /**
+     * Generate a new world name based on the current time
+     *
+     * @param worldName The name of the world to be cloned
+     * @return The new world name
+     */
+    private String generateWorldName(String worldName) {
         long time = Instant.now().getEpochSecond();
         return worldName + "copy" + Long.toString(time);
     }
