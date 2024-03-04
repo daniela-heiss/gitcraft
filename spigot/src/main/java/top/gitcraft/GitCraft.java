d855f0aebc65ab08ba37cfa3243df87a3065cf43
Max Müller <max97m2@gmail.com>
2024-03-04 10:09:18 +0100

    use register methods for readability

--- spigot/src/main/java/top/gitcraft/GitCraft.java
+++ spigot/src/main/java/top/gitcraft/GitCraft.java
@@ -1,8 +1,6 @@
 package top.gitcraft;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import top.gitcraft.commands.TestAreaVisualizer;
-import top.gitcraft.commands.WETestCommand;
 import top.gitcraft.commands.areaselect.GetAreaCommand;
 import top.gitcraft.commands.areaselect.SetPos1Command;
 import top.gitcraft.commands.areaselect.SetPos2Command;
@@ -27,33 +25,14 @@ public final class GitCraft extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        getLogger().info("Hello, SpigotMC!");
+        registerMenuCommands();
+        registerWorldCommands();
 
-        Objects.requireNonNull(this.getCommand("gcmenu")).setExecutor(new MainMenuCommand());
-        Objects.requireNonNull(this.getCommand("gcworldmenu")).setExecutor(new WorldMenuCommand());
-        Objects.requireNonNull(this.getCommand("gclist")).setExecutor(new WorldCommand());
-        Objects.requireNonNull(this.getCommand("gcjoin")).setExecutor(new JoinCommand());
-        Objects.requireNonNull(this.getCommand("gccreate")).setExecutor(new CreateCommand());
-        Objects.requireNonNull(this.getCommand("gcdelete")).setExecutor(new DeleteCommand());
-        Objects.requireNonNull(this.getCommand("gcWETest")).setExecutor(new WETestCommand());
+        registerMergeCommands();
+        registerSchematicCommands();
 
-        Objects.requireNonNull(this.getCommand("automerge")).setExecutor(new AutoMergeCommand());
-        Objects.requireNonNull(this.getCommand("areamerge")).setExecutor(new AreaMergeCommand());
-        Objects.requireNonNull(this.getCommand("generateschematic")).setExecutor(new GenerateSchematicCommand());
-        Objects.requireNonNull(this.getCommand("pasteschematic")).setExecutor(new PasteSchematicCommand());
-
-        Objects.requireNonNull(this.getCommand("gcWETest")).setExecutor(new WETestCommand());
-
-        // area select
-        Objects.requireNonNull(this.getCommand("gcSetPos1")).setExecutor(new SetPos1Command());
-        Objects.requireNonNull(this.getCommand("gcSetPos2")).setExecutor(new SetPos2Command());
-        Objects.requireNonNull(this.getCommand("gcGetSelection")).setExecutor(new GetAreaCommand());
-
-        Objects.requireNonNull(this.getCommand("testareavisulize")).setExecutor(new TestAreaVisualizer());
-        //save-load
-        Objects.requireNonNull(this.getCommand("gcload")).setExecutor(new LoadCommand());
-        Objects.requireNonNull(this.getCommand("gcsave")).setExecutor(new SaveCommand());
-
+        registerAreaSelectCommands();
+        registerSaveLoadCommands();
 
         getServer().getPluginManager().registerEvents(new AreaSelectListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
@@ -63,5 +42,38 @@ public final class GitCraft extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Goodbye, SpigotMC!");
+    }
+
+    public void registerAreaSelectCommands() {
+        Objects.requireNonNull(this.getCommand("gcSetPos1")).setExecutor(new SetPos1Command());
+        Objects.requireNonNull(this.getCommand("gcSetPos2")).setExecutor(new SetPos2Command());
+        Objects.requireNonNull(this.getCommand("gcGetSelection")).setExecutor(new GetAreaCommand());
+    }
+
+    public void registerSaveLoadCommands() {
+        Objects.requireNonNull(this.getCommand("gcload")).setExecutor(new LoadCommand());
+        Objects.requireNonNull(this.getCommand("gcsave")).setExecutor(new SaveCommand());
+    }
+
+    public void registerMergeCommands() {
+        Objects.requireNonNull(this.getCommand("automerge")).setExecutor(new AutoMergeCommand());
+        Objects.requireNonNull(this.getCommand("areamerge")).setExecutor(new AreaMergeCommand());
+    }
+
+    public void registerSchematicCommands() {
+        Objects.requireNonNull(this.getCommand("generateschematic")).setExecutor(new GenerateSchematicCommand());
+        Objects.requireNonNull(this.getCommand("pasteschematic")).setExecutor(new PasteSchematicCommand());
+    }
+
+    public void registerMenuCommands() {
+        Objects.requireNonNull(this.getCommand("gcmenu")).setExecutor(new MainMenuCommand());
+        Objects.requireNonNull(this.getCommand("gcworldmenu")).setExecutor(new WorldMenuCommand());
+    }
+
+    public void registerWorldCommands() {
+        Objects.requireNonNull(this.getCommand("gclist")).setExecutor(new WorldCommand());
+        Objects.requireNonNull(this.getCommand("gcjoin")).setExecutor(new JoinCommand());
+        Objects.requireNonNull(this.getCommand("gccreate")).setExecutor(new CreateCommand());
+        Objects.requireNonNull(this.getCommand("gcdelete")).setExecutor(new DeleteCommand());
     }
 }
