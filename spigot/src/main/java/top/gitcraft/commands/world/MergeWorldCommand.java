4f34b1aa3d71922bfcb6ac53504b9c929c9c9541
Daniela Heiß <inf3741@hs-worms.de>
2024-03-04 11:19:02 +0100

    New function createWorldSendCallback for MergeWorldCommand

--- spigot/src/main/java/top/gitcraft/commands/world/MergeWorldCommand.java
+++ spigot/src/main/java/top/gitcraft/commands/world/MergeWorldCommand.java
@@ -3,6 +3,7 @@ package top.gitcraft.commands.world;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
 import com.onarandombox.MultiverseCore.*;
+import top.gitcraft.GitCraft;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldType;
@@ -20,5 +21,20 @@ public class MergeWorldCommand {
 
 
 
+    }
+
+    public static void createWorldSendCallback(String worldName, int layerheight, Runnable callback) {
+        MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
+        MVWorldManager worldManager = core.getMVWorldManager();
+
+        //adding the world creation
+
+        Bukkit.getScheduler().runTask(GitCraft.getPlugin(GitCraft.class), () -> {
+            worldManager.addWorld(worldName, World.Environment.NORMAL, null, WorldType.NORMAL, false, "gitcraft." + layerheight);
+            if (callback != null) {
+                callback.run();
+            }
+
+        });
     }
 }
