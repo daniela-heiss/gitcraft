249779751fb5296709255b81d16e5728637a8957
Jurij-Christopher Becker <inf3732@hs-worms.de>
2024-03-05 09:41:21 +0100

    made N bigger

--- spigot/src/main/java/top/gitcraft/commands/world/createVoidWorldCommand.java
+++ spigot/src/main/java/top/gitcraft/commands/world/createVoidWorldCommand.java
@@ -29,7 +29,7 @@ public class createVoidWorldCommand implements CommandExecutor {
         MultiverseCore core = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("Multiverse-Core");
         MVWorldManager worldManager = core.getMVWorldManager();
         WorldPurger purger = worldManager.getTheWorldPurger();
-        String newname = "Merge" + Instant.now().getEpochSecond(); //generating new name for the mergeworld
+        String newName = "Merge" + Instant.now().getEpochSecond(); //generating new name for the mergeworld
         //change gamerules on creation completion with a callback
         Runnable callback = () -> {
             MultiverseWorld mergeWorld = worldManager.getMVWorld(newname);
