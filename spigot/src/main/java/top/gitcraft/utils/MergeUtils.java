34f1e6cadb5eee0bd063e84aa947d337211c1af0
Max Müller <max97m2@gmail.com>
2024-03-06 12:02:21 +0100

    use region methods for size

--- spigot/src/main/java/top/gitcraft/utils/MergeUtils.java
+++ spigot/src/main/java/top/gitcraft/utils/MergeUtils.java
@@ -3,28 +3,22 @@ package top.gitcraft.utils;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.extent.clipboard.BlockArrayClipboard;
 import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.world.World;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
-import static top.gitcraft.utils.SchematicUtils.createClipboard;
+import static top.gitcraft.utils.SchematicUtils.*;
 
 public class MergeUtils {
 
-    public static BlockVector3 calculateSize(BlockVector3 startOrigin, BlockVector3 endOrigin) {
-
-        int x = endOrigin.getX() - startOrigin.getX();
-        int y = endOrigin.getY() - startOrigin.getY();
-        int z = endOrigin.getZ() - startOrigin.getZ();
-
-        return BlockVector3.at(x, y, z);
-    }
 
     // Calculates three areas based on the player's current location in the world
-    public static MergeMetaData calculateAreaCoordinates(Player player, BlockVector3 startOrigin, BlockVector3 endOrigin) {
+    public static MergeMetaData calculateAreaCoordinates(Player player, CuboidRegion region) {
 
-        BlockVector3 size = calculateSize(startOrigin, endOrigin);
+        int yLength = region.getHeight();
+        int zLength = region.getLength();
 
         Location playerCoordinates = player.getLocation();
         double playerCoordinateX = playerCoordinates.getX();
@@ -35,23 +29,33 @@ public class MergeUtils {
         int bufferSizeZ = 5;
 
         // Area on the player's left
-        BlockVector3 areaOriginal = BlockVector3.at(playerCoordinateX + bufferSizeX, playerCoordinateY, (playerCoordinateZ - ((double) size.getY() / 2)) - bufferSizeZ - size.getZ());
+        BlockVector3 areaOriginal = BlockVector3.at(playerCoordinateX + bufferSizeX, playerCoordinateY, (playerCoordinateZ - ((double) yLength / 2)) - bufferSizeZ - zLength);
 
         // Area right in front of the player
-        BlockVector3 areaChanges = BlockVector3.at(playerCoordinateX + bufferSizeX, playerCoordinateY, playerCoordinateZ - ((double) size.getY() / 2));
+        BlockVector3 areaChanges = BlockVector3.at(playerCoordinateX + bufferSizeX, playerCoordinateY, playerCoordinateZ - ((double) yLength / 2));
 
         // Area on the player's right
-        BlockVector3 areaCombined = BlockVector3.at(playerCoordinateX + bufferSizeX, playerCoordinateY, (playerCoordinateZ + ((double) size.getY() / 2)) + bufferSizeZ + size.getZ());
+        BlockVector3 areaCombined = BlockVector3.at(playerCoordinateX + bufferSizeX, playerCoordinateY, (playerCoordinateZ + ((double) zLength / 2)) + bufferSizeZ + zLength);
 
-        return new MergeMetaData(startOrigin, areaOriginal, areaChanges, areaCombined);
+        return new MergeMetaData(region.getPos1(), areaOriginal, areaChanges, areaCombined);
     }
 
-    public void pasteMergeAreas(Player player, String toWorldName, String fromWorldName, BlockVector3 pos1, BlockVector3 pos2) {
+    public static void pasteMergeAreas(Player player, String targetWorldName, String fromWorldName, String mergeWorldName, CuboidRegion region) {
         World fromWorld = BukkitAdapter.adapt(Bukkit.getWorld(fromWorldName));
-        World toWorld = BukkitAdapter.adapt(Bukkit.getWorld(toWorldName));
+        World targetWorld = BukkitAdapter.adapt(Bukkit.getWorld(targetWorldName));
+        World voidWorld = BukkitAdapter.adapt(Bukkit.getWorld(mergeWorldName));
 
-        BlockArrayClipboard fromClipboard = createClipboard(pos1, pos2, fromWorld, player);
-        BlockArrayClipboard toClipboard = createClipboard(pos1, pos2, toWorld, player);
+        BlockArrayClipboard fromClipboard = createClipboard(region, fromWorld);
+        BlockArrayClipboard targetClipboard = createClipboard(region, targetWorld);
+        BlockArrayClipboard changesClipboard = createClipboardFromChanges(player);
+
+        MergeMetaData coords = calculateAreaCoordinates(player, region);
+
+        pasteClipboard(voidWorld, player, coords.getAreaChanges(), changesClipboard);
+        pasteClipboard(voidWorld, player, coords.getAreaOriginal(), targetClipboard);
+        pasteClipboard(voidWorld, player, coords.getAreaCombined(), fromClipboard);
+
+        TeleportUtils.joinWorldAtCurrentLocation(player, mergeWorldName);
     }
 }
 
