bdce5b62aec80cc70b0735165baede1e318bb334
Moritz Schumann <GreenGryph0n@gmx.de>
2024-03-06 12:01:17 +0100

    added tp for all players to prev world

--- spigot/src/main/java/top/gitcraft/commands/merging/DiscardMergeCommand.java
+++ spigot/src/main/java/top/gitcraft/commands/merging/DiscardMergeCommand.java
@@ -1,10 +1,14 @@
 package top.gitcraft.commands.merging;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import top.gitcraft.utils.WorldUtils;
+
+import java.util.List;
 
 import static top.gitcraft.utils.TeleportUtils.joinWorldAtCurrentLocation;
 
@@ -26,11 +30,21 @@ public class DiscardMergeCommand implements CommandExecutor {
         String previousWorld = args[0];
         String mergeWorldName = player.getWorld().getName();
 
-        // Teleport Player back to his previous world
-        joinWorldAtCurrentLocation(player, previousWorld);
+        if (Bukkit.getWorld(mergeWorldName) == null) return false;
+
+        World currentWorld = Bukkit.getWorld(mergeWorldName);
+
+        // Teleport all players in mergeWorld back to their previous world
+        assert currentWorld != null;
+        List<Player> playersInMergeWorld = currentWorld.getPlayers();
+
+        for(Player players : playersInMergeWorld) {
+            joinWorldAtCurrentLocation(players, previousWorld);
+        }
 
         // Delete player's Merge world
         WorldUtils worldUtils = new WorldUtils();
+
         worldUtils.deleteWorld(player, mergeWorldName);
         worldUtils.logWorldDelete(player, mergeWorldName);
 
