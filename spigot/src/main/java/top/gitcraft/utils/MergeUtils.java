db9807887e1f52ebb0f7b613d1fccc73b26a8e1e
Max Müller <max97m2@gmail.com>
2024-03-06 14:00:30 +0100

    use void world

--- spigot/src/main/java/top/gitcraft/utils/MergeUtils.java
+++ spigot/src/main/java/top/gitcraft/utils/MergeUtils.java
@@ -7,6 +7,8 @@ import com.sk89q.worldedit.regions.CuboidRegion;
 import com.sk89q.worldedit.world.World;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 import org.bukkit.entity.Player;
 
 import static top.gitcraft.utils.SchematicUtils.*;
@@ -41,9 +43,12 @@ public class MergeUtils {
     }
 
     public static void pasteMergeAreas(Player player, String targetWorldName, String fromWorldName, String mergeWorldName, CuboidRegion region) {
+
+        creatVoidWorld(mergeWorldName);
+
         World fromWorld = BukkitAdapter.adapt(Bukkit.getWorld(fromWorldName));
         World targetWorld = BukkitAdapter.adapt(Bukkit.getWorld(targetWorldName));
-        World voidWorld = BukkitAdapter.adapt(Bukkit.getWorld(mergeWorldName));
+        World voidWorld = BukkitAdapter.adapt(creatVoidWorld(mergeWorldName));
 
         BlockArrayClipboard fromClipboard = createClipboard(region, fromWorld);
         BlockArrayClipboard targetClipboard = createClipboard(region, targetWorld);
@@ -56,6 +61,14 @@ public class MergeUtils {
         pasteClipboard(voidWorld, player, coords.getAreaCombined(), fromClipboard);
 
         TeleportUtils.joinWorldAtCurrentLocation(player, mergeWorldName);
+    }
+
+    private static org.bukkit.World creatVoidWorld(String worldName) {
+        WorldCreator wc = new WorldCreator(worldName);
+        wc.type(WorldType.FLAT);
+        wc.generatorSettings("{\"layers\": [{\"block\": \"barrier\", \"height\": 1}], \"biome\":\"desert\"}");
+        wc.generateStructures(false);
+        return wc.createWorld();
     }
 }
 
