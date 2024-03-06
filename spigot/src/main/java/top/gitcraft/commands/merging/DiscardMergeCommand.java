93cd91be8e7e32a800df774edf276a93985095e1
Moritz Schumann <GreenGryph0n@gmx.de>
2024-03-06 10:44:58 +0100

    Added discardMergeCommand

---
+++ spigot/src/main/java/top/gitcraft/commands/merging/DiscardMergeCommand.java
@@ -0,0 +1,40 @@
+package top.gitcraft.commands.merging;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import top.gitcraft.utils.WorldUtils;
+
+import static top.gitcraft.utils.TeleportUtils.joinWorldAtCurrentLocation;
+
+public class DiscardMergeCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("You must be a player to use this command");
+            return false;
+        }
+        Player player = (Player) sender;
+
+        if (args.length != 1) {
+            return false;
+        }
+
+        String previousWorld = args[0];
+        String mergeWorldName = player.getWorld().getName();
+
+        // Teleport Player back to his previous world
+        joinWorldAtCurrentLocation(player, previousWorld);
+
+        // Delete player's Merge world
+        WorldUtils worldUtils = new WorldUtils();
+        worldUtils.deleteWorld(player, mergeWorldName);
+        worldUtils.logWorldDelete(player, mergeWorldName);
+
+
+        return true;
+    }
+}
