208ecd7422c3f5187c294bb32862c2c70f6aef4d
Max Mueller <max97m2@gmail.com>
2024-03-12 14:59:46 +0100

    delete empty merge world

--- spigot/src/main/java/top/gitcraft/listeners/PlayerChangeWorldListener.java
+++ spigot/src/main/java/top/gitcraft/listeners/PlayerChangeWorldListener.java
@@ -1,6 +1,18 @@
 package top.gitcraft.listeners;
 
+import org.bukkit.World;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import top.gitcraft.utils.WorldUtils;
 
 public class PlayerChangeWorldListener implements Listener {
+
+    @EventHandler public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
+        World world = event.getFrom();
+        if (world.getName().startsWith("mergeWorld") && world.getPlayers().isEmpty()) {
+            //check if world is empty
+            WorldUtils.deleteWorld(null, world);
+        }
+    }
 }
