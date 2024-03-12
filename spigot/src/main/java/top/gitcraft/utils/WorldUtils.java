f18bbfba2c966adc37c6347f793dc77c3b07b5f0
Max Mueller <max97m2@gmail.com>
2024-03-12 14:38:16 +0100

    fix copy world

--- spigot/src/main/java/top/gitcraft/utils/WorldUtils.java
+++ spigot/src/main/java/top/gitcraft/utils/WorldUtils.java
@@ -5,9 +5,10 @@ import org.bukkit.entity.Player;
 import top.gitcraft.GitCraft;
 import top.gitcraft.utils.enums.JSONCOLOR;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static top.gitcraft.ui.components.InfoMessages.infoActionWorld;
 import static top.gitcraft.ui.components.InfoMessages.infoWorldAction;
@@ -23,12 +24,48 @@ public class WorldUtils {
      * @param callback         The callback to be executed after the world is cloned
      */
     public static void cloneWorld(String currentWorldName, String newWorldName, Runnable callback) {
-        Bukkit.getServer()
-              .createWorld(new WorldCreator(newWorldName).copy(Bukkit.getWorld(currentWorldName)));
-        callback.run();
+        World originalWorld = Bukkit.getWorld(currentWorldName);
+
+        copyFileStructure(originalWorld.getWorldFolder(),
+                new File(Bukkit.getWorldContainer(), newWorldName));
+        new WorldCreator(newWorldName).createWorld();
 
         if (callback != null) {
             callback.run();
+        }
+    }
+
+
+    private static void copyFileStructure(File source, File target) {
+        try {
+            ArrayList<String> ignore = new ArrayList<>(Arrays.asList("uid.dat", "session.lock"));
+            if (!ignore.contains(source.getName())) {
+                if (source.isDirectory()) {
+                    if (!target.exists()) {
+                        if (!target.mkdirs()) {
+                            throw new IOException("Couldn't create world directory!");
+                        }
+                    }
+                    String[] files = source.list();
+                    for (String file : files) {
+                        File srcFile = new File(source, file);
+                        File destFile = new File(target, file);
+                        copyFileStructure(srcFile, destFile);
+                    }
+                } else {
+                    InputStream in = new FileInputStream(source);
+                    OutputStream out = new FileOutputStream(target);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = in.read(buffer)) > 0) {
+                        out.write(buffer, 0, length);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -76,6 +113,7 @@ public class WorldUtils {
         World mergeWorldBukkit = wc.createWorld();
 
         //set game rules
+        assert mergeWorldBukkit != null;
         mergeWorldBukkit.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
         mergeWorldBukkit.setGameRule(GameRule.DO_MOB_SPAWNING, false);
         mergeWorldBukkit.setGameRule(GameRule.DO_WEATHER_CYCLE, false);
