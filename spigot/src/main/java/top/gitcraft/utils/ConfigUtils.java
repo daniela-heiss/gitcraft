cdb345de06f43400c526251aeed6ae6729ea7825
Moritz Schumann <GreenGryph0n@gmx.de>
2024-03-13 11:23:42 +0100

    Updated Code structure

--- spigot/src/main/java/top/gitcraft/utils/ConfigUtils.java
+++ spigot/src/main/java/top/gitcraft/utils/ConfigUtils.java
@@ -10,38 +10,7 @@ import java.util.Map;
 
 public class ConfigUtils {
 
-    public static void createNewConfigFile () throws FileNotFoundException {
-        Map<String, Object> dataMap = getStringObjectMap();
-
-        String currentDirectory = System.getProperty("user.dir");
-        String folderName = "/plugins/GitCraft/";
-
-        boolean folderCreated = createGitCraftFolder(currentDirectory, folderName);
-        if (folderCreated) {
-            String fileName = "config";
-            String fileEnding = ".yml";
-            File configFile= new File(currentDirectory + "/plugins/GitCraft/" + fileName + fileEnding);
-            if (!configFile.exists()) {
-                // Make it look like a "classic" yaml file
-                DumperOptions options = new DumperOptions();
-                options.setIndent(2);
-                options.setPrettyFlow(true);
-                options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
-
-                PrintWriter writer = new PrintWriter(configFile);
-                Yaml config = new Yaml(options);
-                config.dump(dataMap, writer);
-                System.out.println("config.yml created successfully");
-            } else {
-                System.out.println("config.yml already exists");
-            }
-        } else {
-            System.out.println("Unable to create config.yml, Directory couldn't be created");
-        }
-
-    }
-
-/*
+    /*
     Config-File contents
         --> keepGamemodeOnWorldJoin         : default: false
         --> showParticles                   : default: true
@@ -86,6 +55,37 @@ public class ConfigUtils {
             System.out.println("Error creating " + folderName + " directory");
             return false;
         }
+    }
+
+    public static void createNewConfigFile () throws FileNotFoundException {
+        Map<String, Object> dataMap = getStringObjectMap();
+
+        String currentDirectory = System.getProperty("user.dir");
+        String folderName = "/plugins/GitCraft/";
+
+        boolean folderCreated = createGitCraftFolder(currentDirectory, folderName);
+        if (folderCreated) {
+            String fileName = "config";
+            String fileEnding = ".yml";
+            File configFile= new File(currentDirectory + "/plugins/GitCraft/" + fileName + fileEnding);
+            if (!configFile.exists()) {
+                // Make it look like a "classic" yaml file
+                DumperOptions options = new DumperOptions();
+                options.setIndent(2);
+                options.setPrettyFlow(true);
+                options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+
+                PrintWriter writer = new PrintWriter(configFile);
+                Yaml config = new Yaml(options);
+                config.dump(dataMap, writer);
+                System.out.println("config.yml created successfully");
+            } else {
+                System.out.println("config.yml already exists");
+            }
+        } else {
+            System.out.println("Unable to create config.yml, Directory couldn't be created");
+        }
+
     }
 }
 
